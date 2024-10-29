e338fc8408171e7e52c91c7e417f91d215a7302a
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2024-10-29 18:18:20 -0300

    iniciando projeto java

---
+++ src/main/java/br/edu/lps/misael_otavio/sistema/gerenciamento/aluguel/impressoras/SistemaGerenciamentoAluguelImpressoras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.lps.misael_otavio.sistema.gerenciamento.aluguel.impressoras;
+
+/**
+ *
+ * @author misael
+ */
+public class SistemaGerenciamentoAluguelImpressoras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
