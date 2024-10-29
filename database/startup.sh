45ba441af72c8207290be99634a94d78effd2061
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2024-10-29 17:56:04 -0300

    adicionando gerenciamento de banco de dados

---
+++ database/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SA_PASSWORD="@sa12345"
+##Script to run inside docker container
+arquivos=$(ls /tmp/scripts/*.sql -1)
+for arquivo in $arquivos
+do
+  echo "Executing file: $arquivo"
+  /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i $arquivo -C
+  echo "$arquivo executed successfully"
+done
