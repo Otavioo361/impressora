c950d2c6c4e6fb3b5c676eabb1cbe2794d21e7be
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2024-11-10 16:00:37 -0300

    Renomeando de proponente para cliente

---
+++ database/sql/CREATE/005_CREATE_tipo_cliente.sql
@@ -0,0 +1,13 @@
+IF NOT EXISTS(SELECT 1 FROM sys.tables WHERE NAME = 'tipo_cliente')
+BEGIN
+  CREATE TABLE alugel_impressora.dbo.tipo_cliente (
+    id_tipo_cliente INT NOT NULL IDENTITY(1,1),
+    cd_tipo_cliente INT NOT NULL UNIQUE,
+    nm_tipo_cliente VARCHAR(30) NOT NULL,
+    dt_inclusao DATETIME NOT NULL DEFAULT GETDATE(),
+    dt_alteracao DATETIME NULL,
+    nm_usuario VARCHAR(60) NULL,
+    in_tipo_cliente_ativo BIT NOT NULL,
+    CONSTRAINT PK_tipo_cliente PRIMARY KEY (id_tipo_cliente)
+  );
+END;
