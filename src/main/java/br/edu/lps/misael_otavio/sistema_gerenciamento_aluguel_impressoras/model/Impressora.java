4da19096832d76adc7694aaa84fde7a07dc4b12b
Otávio Pereira Tristão <otaviopere20@hotmail.com>
2025-01-06 13:35:54 -0300

    Novas telas e fluxos

---
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/model/Impressora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model;
+import lombok.Getter;
+import lombok.Setter;
+/**
+ *
+ * @author otavi
+ */
+@Getter 
+@Setter
+public class Impressora {   
+    private int tipoImpressora;
+    private String modeloImpressora;
+    private String cdBarrasImpressora;    
+    private int qtdImpressora;
+    private String statusImpressora;
+}
