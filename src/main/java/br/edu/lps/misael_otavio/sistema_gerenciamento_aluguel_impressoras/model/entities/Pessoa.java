c263534040aa2000b2173fc36105139c7923ab8f
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2025-01-19 19:14:46 -0300

    Removendo dados atuais das models

---
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/model/entities/Pessoa.java
@@ -0,0 +1,48 @@
+package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ *
+ * @author otavi
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "pessoa")
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pessoa", nullable = false, updatable = false)
+    private Long idPessoa;
+
+    @Column(name = "nm_pessoa", nullable = false, length = 100)
+    private String nmPessoa;
+
+    @Column(name = "sexo", nullable = false, length = 1)
+    private String cdSexo;
+
+    @Column(name = "dt_nascimento", nullable = false)
+    private LocalDate dtNascimento;
+
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome +"\n"
+        +"Sexo: "+ this.sexo + "\n"
+        +"Idade: "+ this.idade+"\n";
+    }   
+
+    
+}
