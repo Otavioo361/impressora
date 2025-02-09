8bcf8435c89c27b45d51fe6eb082f3d3b409685d
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2025-02-08 21:02:05 -0300

    Adicionando outras daos

---
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/model/dao/ClienteDao.java
@@ -0,0 +1,82 @@
+package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.dao;
+
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.factory.EntityManagerSingleton;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.factory.LoggerSingleton;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.interfaces.DaoInterface;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.entities.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+import org.slf4j.Logger;
+
+public class ClienteDao implements DaoInterface<Cliente> {
+    private final EntityManager entityManager = EntityManagerSingleton.createEntityManager();
+    private final Logger logger =  LoggerSingleton.getLogger(this.getClass());
+
+    private final String queryBuscaCpfCnpjOUEmail = "SELECT \n" +
+            "\t*\n" +
+            "FROM aluguel_impressora.dbo.cliente c\n" +
+            "INNER JOIN aluguel_impressora.dbo.pessoa p \n" +
+            "\tON p.id_pessoa = c.id_pessoa \n" +
+            "LEFT JOIN aluguel_impressora.dbo.pessoa_fisica pf\n" +
+            "\tON p.id_pessoa_fisica = pf.id_pessoa_fisica\n" +
+            "LEFT JOIN aluguel_impressora.dbo.pessoa_juridica pj \n" +
+            "\tON p.id_pessoa_juridica  = pj.id_pessoa_juridica \n" +
+            "WHERE \n" +
+            "\t(:cpfCnpj IS NOT NULL AND p.cd_cpf_cnpj = :cpfCnpj) \n" +
+            "\tOR (:email IS NOT NULL AND p.cd_email = :email)";
+
+    @Override
+    public void save(Cliente obj) {
+        entityManager.persist(obj);
+    }
+
+    @Override
+    public void update(Cliente obj) {
+        entityManager.merge(obj);;
+    }
+
+    @Override
+    public boolean delete(Cliente obj) {
+        return false;
+    }
+
+    public Cliente findByCpfCnpj(String cpfCnpj) {
+        return this.findByCpnfCpnpjOrEmail(cpfCnpj,null);
+    }
+    public Cliente findByEmail(String email) {
+        return this.findByCpnfCpnpjOrEmail(null, email);
+    }
+    private Cliente findByCpnfCpnpjOrEmail(String cpfCnpj, String email) {
+        TypedQuery<Cliente> qry = this.entityManager.createQuery(this.queryBuscaCpfCnpjOUEmail, Cliente.class);
+        qry.setParameter("cpf", cpfCnpj);
+        qry.setParameter("email", email);
+        return qry.getSingleResult();
+    }
+
+    public Cliente validaClienteExistente(Cliente cliente){
+        return this.findByCpnfCpnpjOrEmail(cliente.getPessoa().getCdCpfCnpj(),cliente.getPessoa().getCdEmail());
+    }
+
+    @Override
+    public Cliente findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Cliente> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public List<Cliente> findPaginated(int page, int pageSize) {
+        return List.of();
+    }
+
+    @Override
+    public List<Cliente> findTop(int top) {
+        return List.of();
+    }
+
+}
