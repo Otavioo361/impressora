830fcf08a0521c3cfea096b2239650af7f7bd8c0
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2025-02-13 10:39:48 -0300

    Atualizando enum de telas para terem campos de identificação e label da tela

--- src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/utils/ScreensName.java
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/utils/ScreensName.java
@@ -1,5 +1,15 @@
 package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ScreensName {
-    CADASTRO_CLIENTE
+    IMPRESSORAS("impressoras","Impressoras"),
+    HOME("home","Inicio"),
+    ;
+    private final String screenName;
+    private final String label;
+
 }
