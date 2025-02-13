1fec8a19a30fad763f66c7cea0255a381bb2c404
Misaruto <misaelg.freitas2000@gmail.com>
2025-02-13 15:01:49 -0300

    Adicionando tratamento de nulos nos formatadores

--- src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/utils/Formatadores.java
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/utils/Formatadores.java
@@ -6,22 +6,35 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Objects;
 
 public class Formatadores {
     public static String formatMoney(BigDecimal value) {
+        if (Objects.isNull(value)){
+            value = new BigDecimal("0.0");
+        }
         NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
         return currencyFormat.format(value);
     }
     public static String formatDate(LocalDate date) {
+        if (Objects.isNull(date)){
+            return "";
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return date.format(formatter);
     }
 
     public static String formatDateTime(LocalDateTime dateTime) {
+        if (Objects.isNull(dateTime)){
+            return "";
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         return dateTime.format(formatter);
     }
-   public static String formatDateTimeDate(LocalDateTime dateTime) {
+    public static String formatDateTimeDate(LocalDateTime dateTime) {
+        if (Objects.isNull(dateTime)){
+            return "";
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return dateTime.format(formatter);
     }
