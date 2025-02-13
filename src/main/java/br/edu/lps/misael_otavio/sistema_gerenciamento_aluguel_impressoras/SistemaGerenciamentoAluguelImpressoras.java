725c451b13ccf9199151972d1949880d4f2ef002
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2025-02-13 10:42:43 -0300

    removendo itens de teste

--- src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/SistemaGerenciamentoAluguelImpressoras.java
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/SistemaGerenciamentoAluguelImpressoras.java
@@ -1,7 +1,5 @@
 package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras;
 
-import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.dao.UsuarioDao;
-import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.service.PasswordService;
 import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.view.template.FrLogin;
 
 public class SistemaGerenciamentoAluguelImpressoras {
@@ -9,7 +7,6 @@ public class SistemaGerenciamentoAluguelImpressoras {
 
 
     public static void main(String[] args) {
-        System.out.println(PasswordService.hash("123"));
         new FrLogin().setVisible(true);
     }
 
