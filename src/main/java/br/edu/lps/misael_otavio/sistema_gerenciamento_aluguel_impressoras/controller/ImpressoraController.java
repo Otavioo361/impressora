219c6259584abdb233b5a5690905c39baaacc832
0t4v10361 <otaviopere20@hotmail.com>
2025-02-14 11:26:30 -0300

    Atualizando controller de impressora, adicionando função de cadastro

--- src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/controller/ImpressoraController.java
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/controller/ImpressoraController.java
@@ -1,23 +1,48 @@
 package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.controller;
 
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.factory.DataResponseFabric;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.factory.LoggerSingleton;
 import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.DataResponseModel;
 import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.dao.ImpressoraDao;
 
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.entities.Impressora;
 import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils.DefaultMessages;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils.transaction.Transactional;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils.valid.ValidateImpressora;
+import org.slf4j.Logger;
 
+import java.util.HashMap;
 import java.util.List;
 
 
 public class ImpressoraController {
-    private static final String name = "Impressora";
+    private static final String name = Impressora.class.getSimpleName();
     private ImpressoraDao impressoraDao = new ImpressoraDao();
+    private ValidateImpressora validateImpressora = new ValidateImpressora();
+    private Logger logger = LoggerSingleton.getLogger(this.getClass());
+    @Transactional
+    public DataResponseModel<Impressora> save(HashMap<String, String> dados) {
+        try {
+            Impressora impressora = validateImpressora.validarCamposEntrada(dados);
+            System.out.println(impressora);
+            impressora = this.impressoraDao.save(impressora);
+            return DataResponseFabric.fabricSuccessResponse(DefaultMessages.CADASTRADO_SUCESSO.formatMessage(name), impressora);
+        } catch (RuntimeException e) {
+            this.logger.error(e.getMessage());
+            e.printStackTrace();
+            return DataResponseFabric.fabricFailResponse(e.getMessage(),e );
+        }
+    }
+
     public DataResponseModel<List<Impressora>> buscarImpressorasRecentes() {
         try {
-            return new DataResponseModel<>(true, DefaultMessages.CONSULTA_SUCESSO.formatMessage(name), this.impressoraDao.listarTop20(),null);
-        } catch (RuntimeException e){
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-            return new DataResponseModel<>(false, DefaultMessages.CONSULTA_ERROR.formatMessage(name),null,e);
+            List<Impressora> impressoras = this.impressoraDao.listarTop20();
+            return DataResponseFabric.fabricSuccessResponse(DefaultMessages.CONSULTA_SUCESSO.formatMessage(name),impressoras);
+
+        } catch (RuntimeException e) {
+            this.logger.error(DefaultMessages.CONSULTA_ERROR.formatMessage(name));
+            this.logger.error(e.getMessage());
+            return DataResponseFabric.fabricFailResponse(DefaultMessages.CONSULTA_ERROR.formatMessage(name),e );
         }
-}
+    }
 }
