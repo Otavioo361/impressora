c7179140f17ed744c7025f170bcdf6b694636df4
Misaruto <misaelg.freitas2000@gmail.com>
2025-02-13 23:49:14 -0300

    Atualizando popUp

--- src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/view/components/PopUpDefault.java
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/view/components/PopUpDefault.java
@@ -1,17 +1,19 @@
 package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.view.components;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PopUpDefault {
     private JDialog modal;
 
     public PopUpDefault(JFrame frame) {
         this.modal = new JDialog(frame, "Mensagem", true);
-        this.modal.setSize(200, 150);
+        this.modal.setSize(400, 300);
         this.modal.setLocationRelativeTo(frame);
     }
     public void showPopUp(String message) {
         JLabel label = new JLabel(message, SwingConstants.CENTER);
+        label.setFont(new Font("Arial",Font.BOLD,18));
         this.modal.add(label);
         JButton closeButton = new JButton("Fechar");
         closeButton.addActionListener(e1 -> modal.dispose());
