d5479303879b992e67d3d3d09442aa98b8701935
0t4v10361 <otaviopere20@hotmail.com>
2025-02-13 23:47:55 -0300

    Atualizando tabela de modelo_consumivel

--- database/sql/CREATE/013_CREATE_modelo_consumivel.sql
+++ database/sql/CREATE/013_CREATE_modelo_consumivel.sql
@@ -8,7 +8,7 @@ BEGIN
     nm_modelo_consumivel VARCHAR(64) NOT NULL,
     qt_consumivel INT NULL,
     qt_consumivel_disponivel INT NULL,
-    qt_consumivel_alugada INT NULL,
+    qt_consumivel_usado INT NULL,
     dt_inclusao DATETIME2(0) NOT NULL DEFAULT GETDATE(),
     dt_alteracao DATETIME2 NULL,
     nm_usuario VARCHAR(60) NULL,
@@ -16,6 +16,6 @@ BEGIN
     CONSTRAINT PK_modelo_consumivel PRIMARY KEY (id_modelo_consumivel),
     CONSTRAINT FK01_modelo_consumivel_X_marca FOREIGN KEY (id_marca_consumivel) REFERENCES aluguel_impressora.dbo.marca(id_marca),
     CONSTRAINT FK02_modelo_consumivel_X_tipo_consumivel FOREIGN KEY (id_tipo_consumivel) REFERENCES aluguel_impressora.dbo.tipo_consumivel(id_tipo_consumivel),
-    CONSTRAINT FK02_modelo_consumivel_X_taxa FOREIGN KEY (id_taxa) REFERENCES aluguel_impressora.dbo.taxa(id_taxa)
+    CONSTRAINT FK03_modelo_consumivel_X_taxa FOREIGN KEY (id_taxa) REFERENCES aluguel_impressora.dbo.taxa(id_taxa)
 );
 END;
