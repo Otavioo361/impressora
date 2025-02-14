e7395e00c7224abedfb536080e9c12f075b81fd7
0t4v10361 <otaviopere20@hotmail.com>
2025-02-14 11:26:02 -0300

    Adicionando novas mensagens para erros de validação de formulário

--- src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/utils/DefaultMessages.java
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/utils/DefaultMessages.java
@@ -1,14 +1,20 @@
 package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils;
 
-public enum DefaultMessages {
+import java.util.List;
 
+public enum DefaultMessages {
+    //SUCESSO
     CONSULTA_SUCESSO("Consulta de %s feita com sucesso!"),
-    CONSULTA_ERROR("Falha ao realizar a consulta de %s!"),
-    LOGIN_NAO_ENCONTRADO("Usuário não encontrado!"),
-    USUARIO_SENHA_INCORRETOS("Email ou senha incorretos!"),
+    CADASTRADO_SUCESSO("%s cadastrado com sucesso!"),
     LOGIN_SUCESSO("Usuário logado com sucesso!"),
-    EMAIL_INVALIDO("O Email informádo é inválido!");
-    
+    //ERROS
+    USUARIO_SENHA_INCORRETOS("Email ou senha incorretos!"),
+    LOGIN_NAO_ENCONTRADO("Usuário não encontrado!"),
+    EMAIL_INVALIDO("O Email informádo é inválido!"),
+    CONSULTA_ERROR("Falha ao realizar a consulta de %s!"),
+    ERRO_FOMULARIO("<html>Falha ao processar formulario.<br> Erros: <br>&nbsp;&nbsp;&nbsp;&nbsp; %s </html>"),
+    ERRO_DADOS_NAO_ENCOTRADO("Operação cancelada, sem dados suficiente.");
+
     private String message;
     DefaultMessages(String message){
         this.message = message;
@@ -19,5 +25,8 @@ public enum DefaultMessages {
     }
     public String formatMessage(String message){
         return String.format(this.getMessage(),message);
+    }
+    public String formatMessage(List<String> message){
+        return String.format(this.getMessage(), String.join("<br>&nbsp;&nbsp;&nbsp;&nbsp;", message));
     }
 }
