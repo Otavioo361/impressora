aa442beb7cd3f920282265703b08e0c5a0a5d8d0
Misaruto <misaelg.freitas2000@gmail.com>
2025-02-15 19:59:30 -0300

    Criando controllers de marca, taxa e tipo impressora para funcionar a tela de cadastro de modelo de impressora

---
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/controller/TaxaController.java
@@ -0,0 +1,68 @@
+package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.controller;
+
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.factory.DataResponseFabric;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.factory.LoggerSingleton;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.DataResponseModel;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.dao.FornecedorDao;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.dao.TaxaDao;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.entities.Fornecedor;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.entities.Taxa;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils.DefaultMessages;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils.valid.ValidateFornecedor;
+import org.slf4j.Logger;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class TaxaController {
+    private static final String name = Taxa.class.getSimpleName();
+    private final TaxaDao taxaDao = new TaxaDao();
+
+    private final Logger logger = LoggerSingleton.getLogger(TaxaController.class);
+
+    public DataResponseModel<Taxa> save(HashMap<String,String> dados) {
+        try {
+            Taxa fornecedor = new Taxa();
+
+            return DataResponseFabric.fabricSuccessResponse(DefaultMessages.CADASTRADO_SUCESSO.formatMessage(name), fornecedor);
+        } catch (RuntimeException e) {
+            this.logger.error(e.getMessage());
+            e.printStackTrace();
+            this.logger.error(Arrays.toString(e.getStackTrace()));
+            return DataResponseFabric.fabricFailResponse(e.getMessage(),e );
+        }
+    }
+
+    public DataResponseModel<List<Taxa>> findAll() {
+        try {
+            List<Taxa> dados = this.taxaDao.findAll();
+            return new DataResponseModel<>(
+                    true,
+                    DefaultMessages.CONSULTA_SUCESSO.formatMessage(name),
+                    dados,
+                    null
+            );
+        } catch (RuntimeException e) {
+            this.logger.error(e.getMessage());
+            this.logger.error(Arrays.toString(e.getStackTrace()));
+            return DataResponseFabric.fabricFailResponse(DefaultMessages.CONSULTA_ERROR.formatMessage(name),e);
+        }
+    }
+
+    public DataResponseModel<List<Taxa>> findActivesOnly() {
+        try {
+            List<Taxa> dados = this.taxaDao.findActivesOnly();
+            return new DataResponseModel<>(
+                    true,
+                    DefaultMessages.CONSULTA_SUCESSO.formatMessage(name),
+                    dados,
+                    null
+            );
+        } catch (RuntimeException e) {
+            this.logger.error(e.getMessage());
+            this.logger.error(Arrays.toString(e.getStackTrace()));
+            return DataResponseFabric.fabricFailResponse(DefaultMessages.CONSULTA_ERROR.formatMessage(name),e);
+        }
+    }
+}
