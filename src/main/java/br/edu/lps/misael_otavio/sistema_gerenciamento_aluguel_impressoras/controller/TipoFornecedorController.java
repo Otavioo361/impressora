9c52ad5c8bb55a6037ca3293d8b6cb67f9cafa86
Misaruto <misaelg.freitas2000@gmail.com>
2025-02-15 16:51:00 -0300

    Adicionando controller e dao de fornecedor

---
+++ src/main/java/br/edu/lps/misael_otavio/sistema_gerenciamento_aluguel_impressoras/controller/TipoFornecedorController.java
@@ -0,0 +1,29 @@
+package br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.controller;
+
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.DataResponseModel;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.dao.TipoFornecedorDao;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.model.entities.TipoFornecedor;
+import br.edu.lps.misael_otavio.sistema_gerenciamento_aluguel_impressoras.utils.DefaultMessages;
+
+import java.util.List;
+
+public class TipoFornecedorController {
+    private static final String name = TipoFornecedor.class.getSimpleName();
+    private final TipoFornecedorDao tipoFornecedorDao = new TipoFornecedorDao();
+
+    public DataResponseModel<List<TipoFornecedor>> findAll() {
+        try {
+            List<TipoFornecedor> dados = this.tipoFornecedorDao.findAll();
+            return new DataResponseModel<>(
+                    true,
+                    DefaultMessages.CONSULTA_SUCESSO.formatMessage(name),
+                    dados,
+                    null
+            );
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return new DataResponseModel<>(false, DefaultMessages.CONSULTA_ERROR.formatMessage(name), null, e);
+        }
+    }
+}
