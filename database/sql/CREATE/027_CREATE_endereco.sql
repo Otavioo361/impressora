9be83f19c995f1fc9b5ad3495696cf253c3ba1cd
Misaruto <misaelg.freitas2000@gmail.com>
2025-02-15 23:55:00 -0300

    Movimentando tabela de endereço para que possa se relacionar com cliente

--- database/sql/CREATE/027_CREATE_endereco.sql
+++ database/sql/CREATE/027_CREATE_endereco.sql
@@ -2,6 +2,7 @@ IF NOT EXISTS(SELECT 1 FROM sys.tables WHERE NAME = 'endereco')
 BEGIN
   CREATE TABLE aluguel_impressora.dbo.endereco (
     id_endereco BIGINT NOT NULL IDENTITY(1,1),
+    id_cliente BIGINT NULL,
     id_uf INT NOT NULL,
     nr_logradouro INT,
     nm_logradouro VARCHAR(120),
@@ -12,7 +13,9 @@ BEGIN
     dt_inclusao DATETIME2(0) NOT NULL DEFAULT GETDATE(),
     dt_alteracao DATETIME NULL,
     nm_usuario VARCHAR(60) NULL,
+    
     CONSTRAINT PK_endereco PRIMARY KEY (id_endereco),
-    CONSTRAINT FK_endereco_x_uf FOREIGN KEY (id_uf) REFERENCES aluguel_impressora.dbo.uf(id_uf)
+    CONSTRAINT FK01_endereco_x_uf FOREIGN KEY (id_uf) REFERENCES aluguel_impressora.dbo.uf(id_uf),
+    CONSTRAINT FK02_endereco_x_cliente FOREIGN KEY (id_cliente) REFERENCES aluguel_impressora.dbo.cliente(id_cliente)
   );
 END;
